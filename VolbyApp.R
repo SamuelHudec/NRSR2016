8f647605074af39f37372e898c2ef09e83fdbfc1
SamuelHudec <39057205+SamuelHudec@users.noreply.github.com>
2018-05-07 16:20:06 +0200

    Add files via upload

---
+++ VolbyApp.R
@@ -0,0 +1,106 @@
+
+library(shiny)
+library(ggplot2)
+library(DT)
+
+volby <- read.table("https://raw.githubusercontent.com/SamuelHudec/NRSR2016/master/volby16.txt", header=TRUE, quote="\"")
+
+
+# Define UI body
+ui <- fluidPage(
+  # main title
+  titlePanel("Prehlad volebnych dat do NRSR 2016"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      
+      strong("Obrazok v pravo sluzi na interaktivnejsie znazornenie volebnych vysledkou"),
+      helpText("Vysvetlovana premenna je zvolena politicka strana vyjadrena v percentach na volebny okrsok"),
+
+      # Input: response
+      selectInput(inputId = "res",
+                  label = "Vysvetlovana premenna:",
+                  choices = c("OlanoNova"="OlanoNova_percent", 
+                    "SmeRodina" = "SmeRodina_percent", 
+                    "Most"="Most_percent",
+                    "SNS"="SNS_percent",
+                    "SMER"="SMER_percent",
+                    "KDH"="KDH_percent",
+                    "LSNS"="LSNS_percent",
+                    "Siet"="Siet_percent",
+                    "SMK"="SMK_percent",
+                    "SaS"="SaS_percent"),
+                  selected = "SMER_percent"),
+      
+      
+      helpText("Vo vztahu k vysvetlujucej premennej mozeme za pomoci interaktivnej vyzulalizacie lepsie pochopit zmyslanie volicov"),
+      
+      # Input: variable
+      selectInput(inputId = "var",
+                  label = "Vysvetlujuca premenna:",
+                  choices = c("Mladi"="mladi",
+                    "Zakladne vzdelanie" ="zakladne_vzdelanie_.",
+                    "Stredoskolske vzdelanie" = "zaklad.stred",
+                    "Vysoko skolske vzdelanie" = "vs_vzdelanie_mgr",
+                    "Bez vzdelania" ="bez_vzdelania_.",
+                    "Pocet obyvatelov"="pocet_obyvatelov_2014",
+                    "Prirastok obyvatelov"="perce_rozdiel_obyv_2014_ku2004",
+                    "Vek vyssi ako 65" ="vek65v",
+                    "Madarska narodnost" = "madarska_narodnost",
+                    "Rimsko katolicka cirkev" ="rim_kat",
+                    "Vek do 14" ="vek0_14",
+                    "Romska mensina" ="perc_romov",
+                    "per_uoz"),
+                  selected = "mladi"),
+      
+      hr(), # little break for better visual 
+      
+      h3("Nastavenia"),
+      checkboxInput(inputId = "check", 
+                  label = "Ofarbit podla kraja",
+                  value = F),
+
+      sliderInput(inputId= "al",
+        label = "Jas:", min=0, max=1,
+        value = 0.5)
+      ),
+    
+    
+    # Main panel for displaying outputs
+    mainPanel(
+      # Output main plot 
+      plotOutput(outputId = "scatterplot", brush="plot_brush"), hr(),
+      helpText("Vyznacenim bodov v obrazku vyplnite tabulku nizsie"),
+      dataTableOutput(outputId = "partytable")
+      )
+    
+  )
+)
+
+# Define server logic
+server <- function(input, output) {
+  output$scatterplot <- renderPlot({
+    if(input$check){
+      ggplot(data=volby, aes_string(y=input$res,x=input$var,col=volby$kod_kraja)) + 
+      geom_point(alpha=input$al) + labs(y="Vysvetlovana premenna", x="Vysvetlujuca premenna") +
+        theme(legend.position="none")
+    }else{
+      ggplot(data=volby, aes_string(y=input$res,x=input$var)) + 
+      geom_point(alpha=input$al) + labs(y="Vysvetlovana premenna", x="Vysvetlujuca premenna")
+    }
+    
+  })
+  
+  output$partytable <- DT::renderDataTable({
+    brushedPoints(volby, input$plot_brush) %>% select(kod_kraja, obvod ,obec)
+  })
+  
+  
+  
+}
+
+# Run the app
+shinyApp(ui = ui, server = server)
+
+
+
